f587993750cc261dc8a6cd09a78eb86b65f1418f
isaddora freitas augusto rosa <isaddorafreitasar@gmail.com>
2021-05-04 22:15:02 -0300

    add return with api regions

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = new express.Router()
+const axios = require('axios');
 
 router.get('', async (req, res) => {
     try {
-        res.status(201).json({ sucess: true })
+        const regions = await axios.get(`http://images.contelege.com.br/poi.json`).then(res => {
+            return res.data;
+        })
+
+      res.status(201).json({ sucess: true, regions })
     } catch (e) {
         res.status(400).json({ sucess: false, error: e })
     }

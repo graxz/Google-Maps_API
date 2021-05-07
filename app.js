e239652c8f89d818e53f541cbf7c36b8e6ef69c8
isaddora freitas augusto rosa <isaddorafreitasar@gmail.com>
2021-05-07 00:56:04 -0300

    add cors to code for requests

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express')
 const homeRoute = require('./routes/home')
 const aboutMeRoute = require('./routes/about_me')
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}   
+
 const app = express()
 
+
+app.use(allowCrossDomain);
 app.use(express.json())
 app.use(homeRoute)
 app.use(aboutMeRoute)
